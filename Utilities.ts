4519c1c556aa1d9e5abee3bd0c2a22e974c07652
JoseyMedley <65869488+JoseyMedley@users.noreply.github.com>
2021-11-21 13:23:53 -0600

    Add /suicide command

--- Utilities.ts
+++ Utilities.ts
@@ -1,26 +1,24 @@
-//General utilities for unaestheticminecraft.com
+/* eslint-disable no-restricted-imports */
+// General utilities for unaestheticminecraft.com
 import { system } from "./example_and_test/bedrockapi-system";
 import { CommandRawText } from "./bdsx/bds/command";
 import { Packet } from "./bdsx/bds/packet";
 import { events } from "./bdsx/event";
 import { MinecraftPacketIds } from "./bdsx/bds/packetids";
+import { command } from "./bdsx/command";
+import { getOriginalNode } from "typescript";
 
 console.log("initializing Utilities");
 
-/*command.register((command, originName)=>{
-    if (command === '/suicide') {
-	    var newCommand = "/kill "+ originName;
-        system.executeCommand(newCommand, result => {});
-        return 0;
-    }
-
-});
-*/
+command.register("suicide", "respawns yourself").overload((param, origin, output) =>{
+    var playername = origin.getName();
+    var newcommand = "/kill " + playername;
+    system.executeCommand(newcommand, () => {});
+}, {});
 
 
 // logs chat
 events.packetBefore(MinecraftPacketIds.Text).on(ev=>{
-    
     console.log(ev.name + ": " + ev.message);
 });
 
