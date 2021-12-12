1ca0933f46f32e4c2d42083130be1c2848d238b2
JoseyMedley <65869488+JoseyMedley@users.noreply.github.com>
2021-12-12 12:20:51 -0600

    Improved Anticheat
    Added fakename patch. Kick players for crasher, fakename, and .give.

--- OpenAnticheat.ts
+++ OpenAnticheat.ts
@@ -8,9 +8,10 @@ import { events } from "./bdsx/event";
 import { float32_t } from "./bdsx/nativetype";
 import { hex, indexOfLine } from "./bdsx/util";
 import { InventoryTransaction } from "./bdsx/bds/inventory";
+import { serverInstance } from "bdsx/bds/server";
 import { Block, BlockSource } from "./bdsx/bds/block";
 import { BlockPos } from "./bdsx/bds/blockpos";
-
+import { DeviceOS } from "bdsx/common";
 console.log("Open Anticheat loaded");
 
 
@@ -50,6 +51,7 @@ events.packetRaw(MinecraftPacketIds.InventoryTransaction).on((ptr, size, ni) => 
             if (ptr.readVarUint() === 99999) {
                 var playername = ni.getActor()?.getName();
                 console.log(playername + " used fake inventory transaction packets");
+                serverInstance.disconnectClient(ni, `I don't .give a shit`);
                 return CANCEL;
             }
         } catch { }
@@ -62,6 +64,7 @@ events.packetBefore(MinecraftPacketIds.PlayerAuthInput).on((pk, ni) => {
     if ((pk.moveX === 4294967296 && pk.moveZ === 4294967296) || (pk.pos.x === 4294967296 && pk.pos.y === 4294967296 && pk.pos.z === 4294967296)) {
         var playername = ni.getActor()?.getName();
         console.log(playername + " used crasher");
+        serverInstance.disconnectClient(ni, `Did you really think that shit would work here`);
         return CANCEL;
     }
 });
@@ -76,4 +79,32 @@ events.entityHurt.on((ev)=>{
     if (dealtDamage>200 && entity!="minecraft:ender_crystal"){
         return CANCEL;
     }
+});
+
+//Fakename patch
+
+const names = new Map<NetworkIdentifier, string>();
+
+events.packetAfter(MinecraftPacketIds.Login).on((pk, ni) => {
+    let connreq = pk.connreq;
+    if (!connreq) return;
+    let cert = connreq.cert;
+    if (connreq.getJsonValue()!.DeviceOS !== DeviceOS.PLAYSTATION) {
+        names.set(ni, cert.getIdentityName());
+    }
+});
+
+events.packetSend(MinecraftPacketIds.PlayStatus).on((pk, ni) => {
+    if (pk.status === 3) {
+        if (names.get(ni)) {
+            if (ni.getActor()!.getName() !== names.get(ni)) {
+                console.log(names.get(ni) + " used fakename");
+                serverInstance.disconnectClient(ni, `Use your real username dipshit`);
+            }
+        }
+    }
+});
+
+events.networkDisconnected.on(ni => {
+    names.delete(ni);
 });
