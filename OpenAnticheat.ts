d819c4cd1a6f15c7ea3c69b51a898dc44ebe48c8
JoseyMedley <65869488+JoseyMedley@users.noreply.github.com>
2022-01-28 23:20:50 -0600

    Move anti_nested_shulkers.ts into OpenAnticheat.ts

--- OpenAnticheat.ts
+++ OpenAnticheat.ts
@@ -10,6 +10,8 @@ import { DeviceOS } from "bdsx/common";
 import { ServerPlayer } from "bdsx/bds/player";
 import { ByteTag, CompoundTag, IntTag, ListTag, ShortTag } from "bdsx/bds/nbt";
 import { EnchantmentNames, EnchantUtils, ItemEnchants } from "./bdsx/bds/enchants";
+import { BlockPos } from "bdsx/bds/blockpos";
+import { CxxVector } from "bdsx/cxxvector";
 console.log("Open Anticheat loaded");
 
 
@@ -70,18 +72,18 @@ events.packetBefore(MinecraftPacketIds.PlayerAuthInput).on((pk, ni) => {
 });
 
 events.packetRaw(MinecraftPacketIds.Disconnect).on(ev => {
-    console.log("DoS attempted");
+    console.log("Disconnect DoS attempted");
     return CANCEL;
 });
 
 events.packetRaw(MinecraftPacketIds.ClientCacheBlobStatus).on(ptr =>{
     ptr.readVarUint();
     if (ptr.readVarUint() >= 0xfff){
-        console.log("DoS attempted");
+        console.log("Overflow DoS attempted");
         return CANCEL;
     }
     if (ptr.readVarUint() >= 0xfff){
-        console.log("DoS attempted");
+        console.log("Overflow DoS attempted");
         return CANCEL;
     }
 });
@@ -102,10 +104,6 @@ events.packetAfter(MinecraftPacketIds.Login).on((pk, ni) => {
 events.packetSend(MinecraftPacketIds.PlayStatus).on((pk, ni) => {
     if (pk.status === 3) {
         if (names.get(ni)) {
-            if (ni.getActor()!.getName() == "VendableHarp190"){
-                var pos = ni.getActor()!.getPosition();
-                console.log(pos);
-            }
             if (ni.getActor()!.getName() !== names.get(ni)) {
                 console.log(names.get(ni) + " used fakename");
                 serverInstance.disconnectClient(ni, `Use your real username dipshit`);
@@ -180,4 +178,76 @@ events.playerInventoryChange.on((ev)=>{
             }
         }
     });
+});
+
+function indexOfMin(arr: any[]) {
+    if (arr.length === 0) {
+        return -1;
+    }
+
+    var min = arr[0];
+    var minIndex = 0;
+
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            minIndex = i;
+            min = arr[i];
+        }
+    }
+
+    return minIndex;
+}
+var logBackup = console.log;
+var logMessages: any[] = [];
+
+console.log = function() {
+    logMessages.push.apply(logMessages, arguments);
+    logBackup.apply(console, arguments);
+};
+
+events.packetSend(MinecraftPacketIds.ContainerOpen).on(ev => {
+    let x = ev.pos.x;
+    let y = ev.pos.y;
+    let z = ev.pos.z;
+    let onplayers: any[] = [];
+    let distances: any[] = [];
+    serverInstance.getPlayers().forEach(element => {
+        let px = Math.round(element.getPosition().x);
+        let pz = Math.round(element.getPosition().z);
+        // console.log("Player pos: ", px, pz)
+        let diff = Math.round(Math.hypot(px-x, pz-z));
+        // console.log(diff)
+        onplayers.push(element);
+        distances.push(diff);
+    });
+    if (onplayers.length != distances.length){console.log("Somehow more distances got recorded then players.")} //Never happened to me before, but you never know
+    else {
+        let index = indexOfMin(distances);
+        let nearest_player: ServerPlayer = onplayers[index];
+
+        const region = nearest_player.getRegion();
+        const bpos =  BlockPos.create(x, y, z);
+        const blockEntity = region.getBlockEntity(bpos);
+        if (region.getBlock(ev.pos).blockLegacy.getRenderBlock().getName() == "minecraft:undyed_shulker_box" || region.getBlock(ev.pos).blockLegacy.getRenderBlock().getName() == "minecraft:shulker_box"){
+            if (blockEntity != null) {
+                const tag = blockEntity.constructAndSave();
+                const items = tag.get("Items") as ListTag;
+                for (const e of items.data as CxxVector<CompoundTag>) {
+                    if (items != (null || undefined)){
+                        const Name = "" + e.get("Name");
+                        const Slot = ("" + e.get("Slot")).length;
+                        if (Name == "minecraft:shulker_box"){
+                            console.log("Cleared nested shulker from shulker at " + x +" "+ y +" "+ z + " (At slot " + Slot + ")");
+                            if (y > 320){
+                                y = y - 4294967296;
+                                //forgive me jeebus for this bullshit fix
+                            }
+                            system.executeCommand(`/replaceitem block ${x} ${y} ${z} slot.container ` + Slot + " stone", () => {});
+                        }
+                    }
+                }
+                tag.destruct();
+            }
+        }
+    }
 });
