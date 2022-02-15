28e10e5e988ace615012c36963e81e2cea33f563
JoseyMedley <65869488+JoseyMedley@users.noreply.github.com>
2022-02-14 22:50:53 -0600

    Clean up code and add welcome message

--- Utilities.ts
+++ Utilities.ts
@@ -1,15 +1,13 @@
 /* eslint-disable no-restricted-imports */
 // General utilities for unaestheticminecraft.com
-import { system } from "./example_and_test/bedrockapi-system";
-import { CommandRawText } from "./bdsx/bds/command";
-import { Packet } from "./bdsx/bds/packet";
 import { events } from "./bdsx/event";
 import { MinecraftPacketIds } from "./bdsx/bds/packetids";
 import { command } from "./bdsx/command";
-import { getOriginalNode } from "typescript";
+import { TextPacket } from "bdsx/bds/packets";
+import { DeviceOS } from "bdsx/common";
 
 console.log("initializing Utilities");
-
+const system = server.registerSystem(0, 0);
 command.register("suicide", "respawns yourself").overload((param, origin, output) =>{
     var playername = origin.getName();
     var newcommand = "/kill " + playername;
@@ -18,9 +16,34 @@ command.register("suicide", "respawns yourself").overload((param, origin, output
 
 
 // logs chat
-events.packetBefore(MinecraftPacketIds.Text).on(ev=>{
+events.packetAfter(MinecraftPacketIds.Text).on(ev=>{
     console.log(ev.name + ": " + ev.message);
 });
 
 
 //check inventory
+/*
+command.register("check-inv", "Swaps player inventories").overload((param, origin, output) =>{
+    console.log("bruh");
+});
+*/
+
+events.packetAfter(MinecraftPacketIds.Login).on((ptr, networkIdentifier, packetId) => {
+    const ip = networkIdentifier.getAddress();
+    const connreq = ptr.connreq;
+    if (connreq === null) return; // wrong client
+    const cert = connreq.cert;
+    const xuid = cert.getXuid();
+    const username = cert.getId();
+
+    // sendLog
+    console.log(`Connection: ${username}> IP=${ip}, XUID=${xuid}, OS=${DeviceOS[connreq.getDeviceOS()] || 'UNKNOWN'}`);
+
+    // sendPacket
+    setTimeout(()=>{
+        const textPacket = TextPacket.create();
+        textPacket.message = "Welcome to unaestheticminecraft.com. Use /suicide to respawn. Most god hacks don't work. Git gud or git out";
+        textPacket.sendTo(networkIdentifier);
+        textPacket.dispose();
+    }, 10000);
+});
